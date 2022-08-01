0f1f4b69c7f2c210a9fd33636b873cfc4f26c1b6
Lucas Duarte da Silva <102393984+LukazDs@users.noreply.github.com>
2022-08-01 19:59:22 -0300

    Update pratica-modelagem-ecomerce.sql

--- pratica-modelagem-ecomerce.sql
+++ pratica-modelagem-ecomerce.sql
@@ -1,5 +1,5 @@
 CREATE TABLE "public.customers" (
-	"id" serial NOT NULL,
+	"id" SERIAL NOT NULL,
 	"name" TEXT NOT NULL,
 	"email" TEXT NOT NULL UNIQUE,
 	"password" TEXT NOT NULL UNIQUE,
@@ -8,10 +8,8 @@ CREATE TABLE "public.customers" (
   OIDS=FALSE
 );
 
-
-
 CREATE TABLE "public.products" (
-	"id" serial NOT NULL,
+	"id" SERIAL NOT NULL,
 	"name" TEXT NOT NULL UNIQUE,
 	"price" FLOAT NOT NULL,
 	"image" TEXT NOT NULL,
@@ -20,59 +18,47 @@ CREATE TABLE "public.products" (
   OIDS=FALSE
 );
 
-
-
 CREATE TABLE "public.clothingCategories" (
-	"id" serial NOT NULL,
-	"name" serial NOT NULL UNIQUE,
+	"id" SERIAL NOT NULL,
+	"name" TEXT NOT NULL,
 	CONSTRAINT "clothingCategories_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
 
-
-
 CREATE TABLE "public.clothingSize" (
-	"id" serial NOT NULL,
-	"name" serial(2) NOT NULL,
+	"id" SERIAL NOT NULL,
+	"name" TEXT NOT NULL,
 	CONSTRAINT "clothingSize_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
 
-
-
 CREATE TABLE "public.clothes" (
-	"id" serial NOT NULL,
+	"id" SERIAL NOT NULL,
 	"name" TEXT NOT NULL,
 	"categoryId" integer NOT NULL,
-	"sizeId" integer NOT NULL,
-	"productId" integer NOT NULL,
+	"sizeId" INTEGER NOT NULL,
+	"productId" INTEGER NOT NULL,
 	CONSTRAINT "clothes_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
 
-
+CREATE TYPE "type" AS ENUM ('created', 'pay', 'delivered', 'canceled'));
 
 CREATE TABLE "public.purchaseInformation" (
-	"id" serial NOT NULL,
-	"date" serial NOT NULL DEFAULT 'now()',
+	"id" SERIAL NOT NULL,
+	"date" DATE NOT NULL DEFAULT now(),
 	"address" TEXT NOT NULL,
-	"customerId" integer NOT NULL,
-	"productId" BINARY NOT NULL,
-	"quantity" integer NOT NULL,
-	"status" TEXT NOT NULL,
+	"customerId" INTEGER NOT NULL,
+	"productId" INTEGER NOT NULL,
+	"quantity" INTEGER NOT NULL,
+	"status" "type" NOT NULL,
 	CONSTRAINT "purchaseInformation_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
-
-
-
-
-
-
 
 ALTER TABLE "clothes" ADD CONSTRAINT "clothes_fk0" FOREIGN KEY ("categoryId") REFERENCES "clothingCategories"("id");
 ALTER TABLE "clothes" ADD CONSTRAINT "clothes_fk1" FOREIGN KEY ("sizeId") REFERENCES "clothingSize"("id");
@@ -80,10 +66,3 @@ ALTER TABLE "clothes" ADD CONSTRAINT "clothes_fk2" FOREIGN KEY ("productId") REF
 
 ALTER TABLE "purchaseInformation" ADD CONSTRAINT "purchaseInformation_fk0" FOREIGN KEY ("customerId") REFERENCES "customers"("id");
 ALTER TABLE "purchaseInformation" ADD CONSTRAINT "purchaseInformation_fk1" FOREIGN KEY ("productId") REFERENCES "products"("id");
-
-
-
-
-
-
-
